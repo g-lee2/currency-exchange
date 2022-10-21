d97015194f7a828bad60ee4f13dc6583eebf65ef
Grace Lee <gracexsnoopy@gmail.com>
2022-10-20 19:30:21 -0700

    Remove fetch api to make api call

--- src/js/exchange.js
+++ src/js/exchange.js
@@ -1,16 +1,19 @@
 export default class Exchange {  
   static getExchangeRate(currencyCode1, currencyCode2, amount) {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currencyCode1}/${currencyCode2}/${amount}`)
-    .then(function(response) {
-      if (!response.ok) {
-        const errorMessage = `${response.status} ${response.statusText}`;
-        throw new Error(errorMessage);
-      } else {
-        return response.json();
-      }
-    })      
-    .catch(function(error) {
-      return error;
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest(); 
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currencyCode1}/${currencyCode2}/${amount}`;
+      request.addEventListener("loadend", function() {
+        const response = JSON.parse(this.responseText)
+        if (this.status === 200) {
+          resolve(response);
+        } else {
+          reject([this, response]);
+        }
+      });
+      request.open("GET", url, true);
+      request.send();
     });
   }
 }
+  
