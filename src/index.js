94db021079a4de2f75e6e6b7dea21fdc6c134d05
Grace Lee <gracexsnoopy@gmail.com>
2022-10-20 17:17:37 -0700

    Start UI logic and display form input/selection

--- src/index.js
+++ src/index.js
@@ -3,10 +3,30 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Exchange from './js/exchange.js';
 
-function handleForm(event) {
-  event.preventDefault();
+function displayApiData(codeCountry1, codeCountry2, exchangeAmount) {
+  let exchangeRateResult = Exchange.getExchangeRate(codeCountry1, codeCountry2, exchangeAmount);
+  //let conversionRate = exchangeRateResult.conversion_rate;
+  console.log(exchangeRateResult);
+  //let conversionResult = exchangeRateResult.conversion_result;
+  //document.getElementById("rate-conversion").innerHTML = conversionRate;
+  //document.getElementById("rate").innerHTML = conversionResult;
 }
 
+function displayData(event) {
+  event.preventDefault();
+  let country1 = document.querySelector("#firstCountry").value;
+  console.log(country1);
+  let country2 = document.querySelector("#secondCountry").value;
+  console.log(country2);
+  let currencyAmount = parseInt(document.getElementById("amount-input").value);
+  console.log(currencyAmount);
+  document.getElementById("code1").innerHTML = country1;
+  document.getElementById("code2").innerHTML = country2;
+  document.getElementById("input-amount").innerHTML = currencyAmount;
+  displayApiData(country1, country2, currencyAmount);
+}
+
+
 window.addEventListener("load", function() {
-  document.querySelector("form").addEventListener("submit", handleForm);
+  document.querySelector("form").addEventListener("submit", displayData);
 });
