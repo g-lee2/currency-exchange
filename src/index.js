1740a0ad1b789debca8b6aaa136597d5fd16c615
Grace Lee <gracexsnoopy@gmail.com>
2022-10-22 11:59:29 -0700

    Change print error function to show unsupported codes error

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './css/styles.css';
 import Exchange from './js/exchange.js';
 
 function getApiData(codeCountry1, codeCountry2, exchangeAmount) {
-  let promise = Exchange.getExchangeRate(codeCountry1, codeCountry2, exchangeAmount);
-  promise.then(function(array) {
-    printElements(array);
-  }, function(errorArray) {
-    printError(errorArray);
-  });
+  Exchange.getExchangeRate(codeCountry1, codeCountry2, exchangeAmount)
+    .then(function(response) {
+      if (response.main) {
+        printElements(response);
+      } else {
+        printError(response);
+      }
+    });
 }
 
 function printElements(data) {
@@ -18,19 +20,25 @@ function printElements(data) {
 }
 
 function printError(error) {
-  document.getElementById('error-message').innerHTML = alert('Something went wrong!');
+  document.getElementById('result').setAttribute('class', 'hidden');
+  if (error.toString().includes('TypeError')) {
+    document.getElementById('error').innerHTML = `${error}! The currency you selected is not supported! Please make sure to make two different selections from the drop down menu!`;
+  } else {
+    document.getElementById('error').innerHTML = `${error}! There was a problem!`;
+  }
 }
+  
 
 function displayData(event) {
   event.preventDefault();
+  document.getElementById('result').removeAttribute('class');
   let country1 = document.querySelector("#firstCountry").value;
-  console.log(country1);
   let country2 = document.querySelector("#secondCountry").value;
-  console.log(country2);
   let currencyAmount = parseInt(document.getElementById("amount-input").value);
-  console.log(currencyAmount);
   document.getElementById("code1").innerHTML = country1;
   document.getElementById("code2").innerHTML = country2;
+  document.getElementById("code1Again").innerHTML = country1;
+  document.getElementById("code2Again").innerHTML = country2;
   document.getElementById("input-amount").innerHTML = currencyAmount;
   getApiData(country1, country2, currencyAmount);
 }
